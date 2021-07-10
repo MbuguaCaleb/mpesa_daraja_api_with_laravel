e852901e67713c9f45b84c0e518913e888837bb7
MbuguaCaleb <mbuguacaleb30@gmail.com>
2021-07-10 15:12:42 +0300

    creeating-a-reusable-function

--- README.md
+++ README.md
@@ -10,6 +10,25 @@ registred the URL(s) first the inform Mpesa.
 
 ```
 
+**Json Encode vs Json decode**
+
+```
+
+Whenever i JSON encode i am converting an array or object to JSON.
+
+When ever in JSON decode i am converting a JSON object into a variable
+
+
+N/B
+
+JSON Is the data Interchange format used in Http.
+
+JSON data is more like a string and not an Object or an array and therefore once
+i receive it i must Docode it.
+
+
+```
+
 **Notes By**
 
 ```
--- app/Http/Controllers/payments/mpesa/MPESAController.php
+++ app/Http/Controllers/payments/mpesa/MPESAController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class MPESAController extends Controller
 {
+
+
+    /**The json_encode() function is an inbuilt function in PHP which is used to convert PHP array or object into JSON representation */
+    /**Whenever i am exteacting data from a response recived from the API i must JSON decode */
+
   
     public function getAccessToken(){
 
@@ -25,11 +30,40 @@ class MPESAController extends Controller
                 CURLOPT_USERPWD => env('MPESA_CONSUMER_KEY') . ':' . env('MPESA_CONSUMER_SECRET')
             )
         );
-        $response = curl_exec($curl);
+
+        /*Convert Json to a variable is returning me data it returns to me JSON*/
+        $response = json_decode(curl_exec($curl));
         curl_close($curl);
 
-        return $response;
+        return $response->access_token;
         //return $response->access_token;
 
+    }
+
+    public function makeHttp($url,$body){
+
+        $curl = curl_init();
+        
+        curl_setopt_array($curl, array(
+          CURLOPT_URL => $url,
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => '',
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 0,
+          CURLOPT_FOLLOWLOCATION => true,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => 'POST',
+          CURLOPT_POSTFIELDS =>json_encode($body),
+          CURLOPT_HTTPHEADER => array(
+            'Authorization: Bearer '.$this->getAccessToken(),
+            'Content-Type: application/json'
+          ),
+        ));
+        
+        $curl_response = curl_exec($curl);
+        
+        curl_close($curl);
+        echo $curl_response;
+        
     }
 }
